38444cbd0edea07ffe02be428421a932425e7440
gombost84 <tamas.gombos84@gmail.com>
2021-04-07 19:21:07 +0200

    Changed the folder structure to a more logical one.

---
+++ Chapter 2/flow_control.py
@@ -0,0 +1,8 @@
+spam = input('Enter a something (1 and 2 give different results): ')
+
+if spam == '1':
+    print('Hello!')
+elif spam == '2':
+    print('Howdy')
+else:
+    print('Greetings!')
---
+++ Chapter 2/flow_control2.py
@@ -0,0 +1,7 @@
+for number in range(1,11):
+    print(number)
+
+number2 = 1
+while number2 < 11:
+    print(number2)
+    number2 += 1
