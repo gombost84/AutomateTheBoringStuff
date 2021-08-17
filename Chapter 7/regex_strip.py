6d5bea23166aa11c3a16f319f36b56ad439115c4
gombost84 <tamas.gombos84@gmail.com>
2021-08-17 19:46:55 +0200

    Lesson 7 further excersises.

---
+++ Chapter 7/regex_strip.py
@@ -0,0 +1,8 @@
+﻿import re
+
+def regex_strip(str, strip = None):
+    if strip == None:
+        print(re.sub(r' (\w) ', r'\1', str))
+
+
+regex_strip(' exactly ')
---
+++ Chapter 7/strong_passowrd.py
@@ -0,0 +1,36 @@
+﻿
+import re
+
+text = """There is one tuple for each match, and each tuple Contains1 strings for each group in the regular expression. Remember that group 0 matches the entire regular expression, so the group at index 0 of the tuple is the one you are interested in.
+"""
+
+def strong_pass_check(pwd):
+    
+    passAlphanum = re.compile(r'\w{8,}')
+
+    passDigit = re.compile(r'[0-9]')
+
+    passLower = re.compile(r'[a-z]')
+
+    passUpper = re.compile(r'[A-Z]')
+    
+    for x in passAlphanum.findall(pwd):
+    
+        if passDigit.search(x) is None:
+            return False
+        
+        elif passLower.search(x) is None:
+            return False
+        
+        elif passUpper.search(x) is None:
+            return False
+
+        else:
+            return True
+
+
+if strong_pass_check(text) == True:
+    print('A OK')
+
+else:
+    print('Nope')
